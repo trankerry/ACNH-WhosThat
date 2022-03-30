82cf5da3d95ca1878bae80cec3515384dc9ad0ed
Kerry <trankerry94@gmail.com>
2022-03-29 21:47:03 -0700

    randomize names/images and get selected answer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import React, { useState } from "react";
-import "./App.css";
-import axios from "axios";
-import logo from './components/logo.jpg';
-import island from './components/island.jpg';
-import ant from './components/ant.png';
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import logo from './images/logo.jpg';
+import island from './images/island.jpg';
 
 function App() {
-	const [first, setFirst] = useState('Name')
-	const [second, setSecond] = useState('Name')
-	const [third, setThird] = useState('Name')
-	const [fourth, setFourth] = useState('Name')
-	const [image, setImage] = useState('https://www.clipartmax.com/png/middle/461-4613415_bachelor-of-fine-arts-animation-cartoon-play-button-png.png')
+	const [first, setFirst] = useState('Name');
+	const [second, setSecond] = useState('Name');
+	const [third, setThird] = useState('Name');
+	const [fourth, setFourth] = useState('Name');
+	const [image, setImage] = useState(
+		'https://www.kindpng.com/picc/m/32-323152_play-button-cartoon-png-transparent-png.png'
+	);
+	const [answer, setAnswer] = useState();
 
-	function handleClick(){
-		axios.get(
-			`http://acnhapi.com/v1a/villagers`
-		).then((res) => {
+	function correctAnswer() {
+		let selectedAnswer = '';
+		let buttons = document.querySelectorAll('button');
+		buttons.forEach((button) => {
+			button.addEventListener('click', () => {
+				selectedAnswer = button.value;
+			});
+		});
+		console.log(answer)
+		console.log(selectedAnswer)
+		if (answer === selectedAnswer) {
+			console.log('hi')
+		}else{
+			console.log('bye');
+		}
+		handleClick();
+	}
+
+	function handleClick() {
+		axios.get(`http://acnhapi.com/v1a/villagers`).then((res) => {
 			let total = res.data.length;
 			let randoms = [];
-			for(let i = 0; i < 4; i++){
-				let num = Math.floor(Math.random() * total)
-				randoms.push(res.data.splice(num,1));
-			};
-			let imageNum = Math.floor(Math.random() * 4)
-			setImage(randoms[imageNum][0]['image_uri'])
+			for (let i = 0; i < 4; i++) {
+				let num = Math.floor(Math.random() * total);
+				randoms.push(res.data.splice(num, 1));
+			}
+			let imageNum = Math.floor(Math.random() * 4);
+			setImage(randoms[imageNum][0]['image_uri']);
+			setAnswer(randoms[imageNum][0]['name']['name-USen']);
 			setFirst(randoms[0][0]['name']['name-USen']);
 			setSecond(randoms[1][0]['name']['name-USen']);
 			setThird(randoms[2][0]['name']['name-USen']);
 			setFourth(randoms[3][0]['name']['name-USen']);
-		})
-	};
+		});
+	}
 
-  return (
+	return (
 		<div className='App'>
 			<div className='container'>
 				<nav className='navbar fixed-top navbar-expand-lg navbar-light bg-light'>
@@ -79,22 +98,38 @@ function App() {
 						<img src={image} alt='character image' className='responsive' />
 					</div>
 					<div className='a1'>
-						<button type='button' onClick={handleClick} className='btn btn-lg btn-info'>
+						<button
+							type='button'
+							onClick={correctAnswer}
+							className='btn btn-lg btn-info'
+							value={first}>
 							{first}
 						</button>
 					</div>
 					<div className='a2'>
-						<button type='button' onClick={handleClick} className='btn btn-lg btn-info'>
+						<button
+							type='button'
+							onClick={correctAnswer}
+							className='btn btn-lg btn-info'
+							value={second}>
 							{second}
 						</button>
 					</div>
 					<div className='a3'>
-						<button type='button' onClick={handleClick} className='btn btn-lg btn-info'>
+						<button
+							type='button'
+							onClick={correctAnswer}
+							className='btn btn-lg btn-info'
+							value={third}>
 							{third}
 						</button>
 					</div>
 					<div className='a4'>
-						<button type='button' onClick={handleClick} className='btn btn-lg btn-info'>
+						<button
+							type='button'
+							onClick={correctAnswer}
+							className='btn btn-lg btn-info'
+							value={fourth}>
 							{fourth}
 						</button>
 					</div>
